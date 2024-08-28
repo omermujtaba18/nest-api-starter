7d0c64288ad41dfa78edce03a173122986df701b
Omer Mujtaba <omer.mujtaba96@gmail.com>
2024-08-28 21:01:16 +0300

    test: fix /health e2e tests

--- src/resources/health/health.controller.ts
+++ src/resources/health/health.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { Health } from './entities/health.entity';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { SkipJwtauth } from '../../common/decorators/skip-jwtauth/skip-jwtauth.decorator';
 
+@SkipJwtauth()
 @ApiTags('health')
 @Controller('health')
 export class HealthController {
--- test/health.e2e-spec.ts
+++ test/health.e2e-spec.ts
@@ -15,6 +15,10 @@ describe('HealthController (e2e)', () => {
     await app.init();
   });
 
+  afterAll(async () => {
+    await app.close();
+  });
+
   it('/health (GET)', () => {
     return request(app.getHttpServer())
       .get('/health')
